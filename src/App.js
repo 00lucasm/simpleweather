7f4ab7cfd21568b8b69ba80f25a19d0f1163a00f
Melo-Lucas <lucs.r06@gmail.com>
2021-04-24 19:12:04 -0300

    Feat: define chamada da API e seleciona os dados consumidos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
+import moment from "moment";
 import { useEffect, useState } from "react";
+import { api } from "./server";
 
 function App() {
   // definição das variáveis de estado com valor inicial
   const [lat, setLat] = useState(0)
   const [long, setLong] = useState(0)
+  const [data, setData] = useState({})
 
   // definição de ciclo de vida do componente
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
+      api.get(`/weather/?lat=${lat}&lon=${long}&units=metric&APPID=273555de924e8c91505f2430c2b00c19`).then(res => {
+        setData(res.data)
+      })
+    }
+
+    fetchData()
+    
   }, [lat, long])
 
   return (
     <div className="App">
-    <h1>
-      lat: {lat}
-      <br/>
-      long: {long}
-    </h1>
+      <div>
+        <h1>City name: { data === undefined ? "Loading..." : data.name}</h1>
+        <h2>Temperature: { data.main === undefined ? "Loading..." : data.main.temp} &deg;C</h2>
+        <h2>Sunrise: { data.sys === undefined ? "Loading..." : new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-US')}</h2>
+        <h2>Sunset: { data.sys === undefined ? "Loading..." : new Date(data.sys.sunset * 1000).toLocaleTimeString('en-US')}</h2>
+        <h2>Description: { data.weather === undefined ? "Loading..." : data.weather[0].main}</h2>
+        <h2>Humidity: { data.main === undefined ? "Loading..." : data.main.humidity} %</h2>
+        <h2>Day: {moment().format('dddd')}</h2>
+        <h2>Date: {moment().format('DD MMM YYYY')}</h2>
+      </div>
     </div>
   );
 }
