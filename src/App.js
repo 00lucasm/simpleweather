77d9310bce1a8a235337c8f84ccc90616f8bca75
Melo-Lucas <lucs.r06@gmail.com>
2021-04-24 18:05:16 -0300

    Feat: cria hook para latitude e longitude

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  // definição das variáveis de estado com valor inicial
+  const [lat, setLat] = useState(0)
+  const [long, setLong] = useState(0)
+
+  // definição de ciclo de vida do componente
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+
+  }, [lat, long])
+
   return (
     <div className="App">
-    //my code comes here
+    <h1>
+      lat: {lat}
+      <br/>
+      long: {long}
+    </h1>
     </div>
   );
 }
